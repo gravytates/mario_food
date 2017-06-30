f49d063e72799b5b7dc5f033d1a866aef497dbd4
Grady L Shelton <gradyish@gmail.com>
2017-06-30 12:05:37 -0700

    pass test for a review body_content that is too short

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -11,4 +11,10 @@ describe Review do
     review = product.reviews.create({author: "a", content_body: "a"  * 251, rating: 1 })
     expect(review.save).to eq(false)
   end
+
+  it("returns false if it's too long of a store name") do
+    product = Product.create!({name: "salt", image: "png", country_origin: "USA"})
+    review = product.reviews.create({author: "a", content_body: "a", rating: 1 })
+    expect(review.save).to eq(false)
+  end
 end
