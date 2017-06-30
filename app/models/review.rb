cd441328bd308108d7b1677b451b46a792b788fa
Grady L Shelton <gradyish@gmail.com>
2017-06-30 12:05:10 -0700

    pass test for a review body_content that is too long

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
   validates :author, :content_body, :rating, presence: true
+  validates :content_body, length: { minimum: 50, maximum: 250 }
+
   belongs_to :product
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -5,4 +5,10 @@ describe Review do
   it { should validate_presence_of :content_body }
   it { should validate_presence_of :rating }
   it { should belong_to :product }
+
+  it("returns false if it's too long of a store name") do
+    product = Product.create!({name: "salt", image: "png", country_origin: "USA"})
+    review = product.reviews.create({author: "a", content_body: "a"  * 251, rating: 1 })
+    expect(review.save).to eq(false)
+  end
 end
