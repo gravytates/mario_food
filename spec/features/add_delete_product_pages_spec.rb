131ddecc439aee8b12d8298dcb4827b8b293338f
Grady L Shelton <gradyish@gmail.com>
2017-06-30 14:19:59 -0700

    pass integration test to delete a product

---
+++ spec/features/add_delete_product_pages_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe "adds and delete a product" do
+  it "will add a product and successfully redirect" do
+    visit products_path
+    click_on "New Product"
+    fill_in "Name", :with => 'Salsa'
+    fill_in "Country origin", :with => 'Canada'
+    fill_in "Cost", :with => '5.99'
+    fill_in 'Image', :with => 'http://food.fnr.sndimg.com/content/dam/images/food/fullset/2011/4/7/0/EA1D01_alton-salsa_s4x3.jpg.rend.hgtvcom.616.462.jpeg'
+    click_on 'Create Product'
+    expect(page).to have_content "Product successfully added!"
+  end
+
+  it 'will delete a product and successfully redirect' do
+    visit products_path
+    click_on "New Product"
+    fill_in "Name", :with => 'Salsa'
+    fill_in "Country origin", :with => 'Canada'
+    fill_in "Cost", :with => '5.99'
+    fill_in 'Image', :with => 'http://food.fnr.sndimg.com/content/dam/images/food/fullset/2011/4/7/0/EA1D01_alton-salsa_s4x3.jpg.rend.hgtvcom.616.462.jpeg'
+    click_on 'Create Product'
+    click_on 'Salsa'
+    click_on 'Delete'
+    expect(page).to have_content "Product successfully removed!"
+  end
+end
