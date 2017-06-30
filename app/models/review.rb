bf6a237b092a5aae590d609899656427ba1cf061
Grady L Shelton <gradyish@gmail.com>
2017-06-30 12:16:10 -0700

    pass tests showing ratings cant be too high or too low

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ApplicationRecord
   validates :author, :content_body, :rating, presence: true
-  validates :content_body, length: { minimum: 50, maximum: 250 }
+  validates :content_body, length: { in: 6..250 }
+  validates :rating, numericality: { greater_than: 0, less_than: 6 }
 
   belongs_to :product
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -6,15 +6,27 @@ describe Review do
   it { should validate_presence_of :rating }
   it { should belong_to :product }
 
-  it("returns false if it's too long of a store name") do
+  it("returns false if it's too long of a review") do
     product = Product.create!({name: "salt", image: "png", country_origin: "USA"})
     review = product.reviews.create({author: "a", content_body: "a"  * 251, rating: 1 })
     expect(review.save).to eq(false)
   end
 
-  it("returns false if it's too long of a store name") do
+  it("returns false if it's too short of a review") do
     product = Product.create!({name: "salt", image: "png", country_origin: "USA"})
     review = product.reviews.create({author: "a", content_body: "a", rating: 1 })
+    expect(review.save).to eq(false)
+  end
+
+  it("returns false if it's too high a rating") do
+    product = Product.create!({name: "salt", image: "png", country_origin: "USA"})
+    review = product.reviews.create({author: "a", content_body: "a" * 200, rating: 6 })
+    expect(review.save).to eq(false)
+  end
+
+  it("returns false if it's too low a rating") do
+    product = Product.create!({name: "salt", image: "png", country_origin: "USA"})
+    review = product.reviews.create({author: "a", content_body: "a" * 200, rating: 0 })
     expect(review.save).to eq(false)
   end
 end
